4d0328ccc2787ce9f08562dcb25e3fa4d8030984
Benjamin Lobos <blobosle@purdue.edu>
2025-11-21 14:45:05 -0500

    Updated fetcher

--- src/wasm/image_fetcher.js
+++ src/wasm/image_fetcher.js
@@ -83,7 +83,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp48sfc_38.js
+// include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp2ayt8fhc.js
 
   if (!Module['expectedDataFileDownloads']) Module['expectedDataFileDownloads'] = 0;
   Module['expectedDataFileDownloads']++;
@@ -216,21 +216,21 @@ Module['FS_createPath']("/", "screenshots", true, true);
 
   })();
 
-// end include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp48sfc_38.js
-// include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmps52na6_0.js
+// end include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp2ayt8fhc.js
+// include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmpnkyouvfl.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if ((typeof ENVIRONMENT_IS_WASM_WORKER != 'undefined' && ENVIRONMENT_IS_WASM_WORKER) || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD) || (typeof ENVIRONMENT_IS_AUDIO_WORKLET != 'undefined' && ENVIRONMENT_IS_AUDIO_WORKLET)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmps52na6_0.js
-// include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp08yzwjfn.js
+  // end include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmpnkyouvfl.js
+// include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp85l2tgpy.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp08yzwjfn.js
+  // end include: /var/folders/52/mw0_gqm978g3flq1bl9lm2w80000gn/T/tmp85l2tgpy.js
 
 
 var arguments_ = [];
